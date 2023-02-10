19f89ead3d00222d58b0f5d4bf9aa6a02a5a0916
LORD KECHIN MENDEZ <119912596+LkennM@users.noreply.github.com>
2023-02-10 11:41:30 +0800

    Create GroceryItem.java

---
+++ Exercise 6/GroceryItem.java
@@ -0,0 +1,13 @@
+public class GroceryItem {
+    String name;
+    double price;
+    
+    public void showItemName(){
+        System.out.println(this.name);    
+    }
+
+    public void showPrice(){
+        System.out.println(this.price);
+    }
+    
+}
