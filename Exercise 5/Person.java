d718a89bc1844f4f085c7badc909401cf89ca940
LORD KECHIN MENDEZ <119912596+LkennM@users.noreply.github.com>
2023-02-10 10:07:08 +0800

    Create Person.java

---
+++ Exercise 5/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+    
+    private String name;
+    private int ccnumber;
+
+    // get() method
+    public String getName() {
+
+        return this.name;
+    }
+
+    // set() method
+    public void setName(String newName) {
+
+        this.name = newName;
+    }
+    public int getCCnumber() {
+
+        return this.ccnumber;
+    }
+
+    // set() method
+    public void setCCnumber(int cardnumber) {
+
+        this.ccnumber = cardnumber;
+
+    }
+
+}
